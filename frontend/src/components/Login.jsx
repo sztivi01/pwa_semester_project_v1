425cba02ed64b43653ad5a03709e94efcebc4de0
klar2161 <56342242+klar2161@users.noreply.github.com>
2022-03-29 10:44:07 +0200

    Login kinda done

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import loginImage from "../assets/background.jpg";
+import {FaUserAlt, FaLock} from 'react-icons/fa';
 
 export default function Login(){
     return (
@@ -8,18 +9,20 @@ export default function Login(){
 
             <div className="flex justify-center items-center h-full" > 
                 <form className='max-w-[400px] w-full mx-auto bg-white p-8'>
-                    <h2>BRAND.</h2>
-                    <div>
-                        <label>Username</label>
-                        <input type='text'/>
+                    <h2 className='text-3xl font-bold text-center py-5 mb-5'>BRAND.</h2>
+                    <div className='flex flex-col mb-4'>
+                        <label className='flex item-center'><FaUserAlt className='ml-1 mr-2 mt-1'/>Username</label>
+                        <input className='border rounded-lg relative bg-gray-100 p-2' type="text" />
                     </div>
-                    <div>
-                        <label>Password</label>
-                        <input type='password'/>
+                    <div className='flex flex-col mb-4'>
+                        <label className='flex item-center'><FaLock className='ml-1 mr-2 mt-1'/>Password</label>
+                        <input className='border rounded-lg relative bg-gray-100 p-2' type="password"/>
                     </div>
-                    <button>Login</button>
-                    <p><input type="checkbox" />Remember Me</p>
-                    <p>Not a member? Sign Up here!</p>
+                    <button className='border rounded-lg w-full py-3 mt-8 bg-indigo-600 hover:bg-indigo-500 relative text-white'>Login</button>
+                    <p className='flex items-center mt-2'><input className='mr-2' type="checkbox" />Remember Me</p>
+                    <button className='underline underline-offset-2 relative mt-8 w-full'>Not a member? Sign Up here!</button>
+                    
+
                 </form>
             </div>
         </div>
