d69b2815ca43bb2ab5b9d5c62c7d2497e427d436
klar2161 <56342242+klar2161@users.noreply.github.com>
2022-05-05 20:24:12 +0200

    next

--- frontend/src/components/RegistrationUser.jsx
+++ frontend/src/components/RegistrationUser.jsx
@@ -4,7 +4,7 @@ import loginImage from "../assets/background.jpg";
 import {FaUserAlt, FaLock} from 'react-icons/fa';
 import {MdEmail} from 'react-icons/md';
 
-async function registerUser(firstName,lastName,username,password) {
+async function registerUser() {
     return fetch('https://www.mecallapi.com/api/login', {
       method: 'POST',
       headers: {
@@ -49,15 +49,18 @@ export default function Register(){
                     timer: 2000,
                   })
             } else {
-                 /*toast.error(error.message + ', please try again!', {
-            position: "top-right",
-            autoClose: false,
-            hideProgressBar: true,
-            closeOnClick: true,
-            pauseOnHover: false,
-            draggable: true,
-            progress: undefined,
-            });*/
+                toast.error(error.message + ', please try again!', {
+                position: "top-right",
+                autoClose: false,
+                hideProgressBar: true,
+                closeOnClick: true,
+                pauseOnHover: false,
+                draggable: true,
+                progress: undefined, 
+            });
+            }
+        }   catch (error) {
+                console.log(error);
             }
         
 
@@ -66,26 +69,26 @@ export default function Register(){
             <img className='absolute w-full h-full object-cover mix-blend-overlay' src={loginImage} alt="/" />
 
             <div className="flex justify-center items-center h-full" > 
-                <form className='max-w-[450px] w-full mx-auto my-20 bg-white p-8'>
+                <form onSubmit={handleSubmit} className='max-w-[450px] w-full mx-auto my-20 bg-white p-8'>
                     <h2 className='text-3xl font-bold text-center py-5 mb-5'>Tasker™</h2>
                     <div className='flex flex-col mb-4'>
                         <label className='flex item-center'><FaUserAlt className='ml-1 mr-2 mt-1'/>First Name</label>
-                        <input className='border border-indigo-600 rounded-lg relative  p-2' type="text" />
+                        <input className='border border-indigo-600 rounded-lg relative  p-2' type="text" value={firstName} onChange={(e) => setfirstName(e.target.value)}/>
                     </div>
                     <div className='flex flex-col mb-4'>
                         <label className='flex item-center'><FaUserAlt className='ml-1 mr-2 mt-1'/>Last Name</label>
-                        <input className='border border-indigo-600 rounded-lg relative  p-2' type="text" />
+                        <input className='border border-indigo-600 rounded-lg relative  p-2' type="text"value= {lastName} onChange={(e) => setlastName(e.target.value)} />
                     </div>
                     <div className='flex flex-col mb-4'>
                         <label className='flex item-center'><MdEmail className='ml-1 mr-2 mt-1'/>Email</label>
-                        <input className='border border-indigo-600 rounded-lg relative  p-2' type="text" />
+                        <input className='border border-indigo-600 rounded-lg relative  p-2' type="text" value={username} onChange={(e) => setUserName(e.target.value)}/>
                     </div>
                     <div className='flex flex-col mb-4'>
                         <label className='flex item-center'><FaLock className='ml-1 mr-2 mt-1'/>Password</label>
-                        <input className='border border-indigo-600 rounded-lg relative  p-2' type="password"/>
+                        <input className='border border-indigo-600 rounded-lg relative  p-2' type="password" value={password} onChange={(e) => setPassword(e.target.value)}/>
                     </div>
                     
-                    <button className='border rounded-lg w-full py-3 mt-8 bg-indigo-600 hover:bg-indigo-500 relative text-white'>Sign Up</button>
+                    <button type='submit' className='border rounded-lg w-full py-3 mt-8 bg-indigo-600 hover:bg-indigo-500 relative text-white'>Sign Up</button>
                     
                     <Link to='/login'>
                     <button className='underline underline-offset-2 relative mt-8 w-full text-indigo-800'>Already a member? Login here!</button>
@@ -95,4 +98,4 @@ export default function Register(){
             </div>
         </div>
     )
-}
+}}
