941001285ffa69e6ea588a4e7672de2bb99eb803
klar2161 <56342242+klar2161@users.noreply.github.com>
2022-05-17 19:45:47 +0200

    settings page design

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -6,6 +6,7 @@ import { IconContext } from "react-icons";
 import * as FaIcons from "react-icons/fa";
 import * as IoIcons from "react-icons/io";
 import "../index.css";
+import avatarImg from "../assets/avatar.jpg";
 
 function Navbar() {
   const [sidebar, setSidebar] = useState(false);
@@ -25,6 +26,13 @@ function Navbar() {
           <Link to="#" className="menu-bars">
             <FaIcons.FaBars onClick={showSidebar} />
           </Link>
+          <div className="items-center justify-end flex-1 hidden space-x-4 sm:flex">
+            <img
+              className="w-16 rounded-full mr-5"
+              src={avatarImg}
+              alt="/"
+            />
+          </div>
         </div>
         <nav className={sidebar ? "nav-menu active" : "nav-menu"}>
           <ul className="nav-menu-items" onClick={showSidebar}>
