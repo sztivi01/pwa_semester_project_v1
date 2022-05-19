dd44ea8e32f15db064c943a15fa0b9351fc83577
klar2161 <56342242+klar2161@users.noreply.github.com>
2022-05-19 15:10:24 +0200

    design of the project tasks and making update for project name and description
    (create doesnt work yet)

--- frontend/src/components/NavbarProjectNames.jsx
+++ frontend/src/components/NavbarProjectNames.jsx
@@ -1,26 +1,32 @@
 import { useQuery } from "react-query";
 import { request } from "../utils/axios-util";
 import { Link } from "react-router-dom";
-import "../index.css"
+import "../index.css";
 
 const fetchProjectNames = () => {
-    return request({ url: "/projects/" + localStorage.getItem("user") });
-  };
-  
+  return request({ url: "/projects/" + localStorage.getItem("user") });
+};
 
-export default function ProjectData(){
-    const { isLoading, data } = useQuery("projectNamesOnNav", fetchProjectNames);
-    if (isLoading) {
-      return <h2>Loading...</h2>;
-    } return (
+export default function ProjectData() {
+  const { isLoading, data } = useQuery("projectNamesNav", fetchProjectNames);
+  if (isLoading) {
+    return <h2>Loading...</h2>;
+  }
+  return (
     <>
-       {data?.data.map((project) => ( 
-         <li key={project._id} className="flex justify-start items-center py-2 pr-0 pl-4 list-none h-14 text-indigo-400">
-             <Link className="flex items-center h-full bg-indigo-700 w-11/12 rounded hover:bg-indigo-900" to={`/project/${project._id}/tasks`}>
-             <span className="ml-4">{project.name}</span>
-             </Link>
-      </li>
-      ))
-       }</>
-    );
-  };
+      {data?.data.map((project) => (
+        <li
+          key={project._id}
+          className="flex justify-start items-center py-2 pr-0 pl-4 list-none h-14 text-indigo-400"
+        >
+          <Link
+            className="flex items-center h-full bg-indigo-700 w-11/12 rounded hover:bg-indigo-900"
+            to={`/project/${project._id}/tasks`}
+          >
+            <span className="ml-4">{project.name}</span>
+          </Link>
+        </li>
+      ))}
+    </>
+  );
+}
--- frontend/src/pages/SpecProject/ListOfTaskByProjectId.jsx
+++ frontend/src/pages/SpecProject/ListOfTaskByProjectId.jsx
@@ -1,101 +1,136 @@
 import { useQuery } from "react-query";
 import { request } from "../../utils/axios-util";
 import { useParams } from "react-router-dom";
+import PopupProjectUpdate from "../../components/PopupProjectUpdate";
 import React, { useEffect, useState } from "react";
 import Item from "../../components/Item";
 import DropWrapper from "../../components/DropWrapper";
 import Col from "../../components/Col";
 import { statuses } from "../../data";
-import './SpecProject.css';
-import SubbmitCard from './SubbmitCard';
-
+import "./SpecProject.css";
+import SubbmitCard from "./SubbmitCard";
 
 
 const fetchTasksByProjectId = (projectId) => {
-    //return axios.get('https://stark-forest-32910.herokuapp.com/api/project')
-    return request({ url: `/tasks/project/${projectId}/tasks` });
-}
+  //return axios.get('https://stark-forest-32910.herokuapp.com/api/project')
+  return request({ url: `/tasks/project/${projectId}/tasks` });
+};
 export const ListOfTaskByProjectId = () => {
-    const [items, setItems] = useState([]);
-    const { projectId } = useParams();
-    const { isLoading,data,refetch } = useQuery('projectNames', () => fetchTasksByProjectId(projectId),{manual:true});
+  const [items, setItems] = useState([]);
+  const { projectId } = useParams();
 
-    useEffect(() => {
-        let tasks = data?.data
-        // do some checking here to ensure data exist
-        if (tasks) {
-            // mutate data if you need to
-            setItems(tasks)
-        }
-    }, [data])
+  const { isLoading, data, refetch } = useQuery(
+    "projectNames",
+    () => fetchTasksByProjectId(projectId),
+    { manual: true }
+  );
 
-    if (isLoading) {
-        return <h2>Loading...</h2>
+
+
+  useEffect(() => {
+    let tasks = data?.data;
+    // do some checking here to ensure data exist
+    if (tasks) {
+      // mutate data if you need to
+      setItems(tasks);
     }
+  }, [data]);
 
-    const onDrop = (item, monitor, status) => {
-        const targetStatus = statuses.find(si => si.status === status);
-        // set targetstatus on the item
-        item.status = targetStatus.status
-        //db call, update item in db based on id
-        request({ url: `tasks/${item._id}`, method: "PUT", data: item })
+  if (isLoading) {
+    return <h2>Loading...</h2>;
+  }
 
-        setItems(prevState => {
-            let changedItem = prevState.find(i => i._id === item._id)
+  const onDrop = (item, monitor, status) => {
+    const targetStatus = statuses.find((si) => si.status === status);
+    // set targetstatus on the item
+    item.status = targetStatus.status;
+    //db call, update item in db based on id
+    request({ url: `tasks/${item._id}`, method: "PUT", data: item });
 
-            if (changedItem) {
-                changedItem.status = targetStatus.status
-            }
-            return [...prevState];
-        });
-    };
-    const moveItem = (dragIndex, hoverIndex) => {
-        const item = items[dragIndex];
-        item.order = hoverIndex
-        request({ url: `tasks/${item._id}`, method: "PUT", data: item })
+    setItems((prevState) => {
+      let changedItem = prevState.find((i) => i._id === item._id);
 
-        setItems(prevState => {
+      if (changedItem) {
+        changedItem.status = targetStatus.status;
+      }
+      return [...prevState];
+    });
+  };
+  const moveItem = (dragIndex, hoverIndex) => {
+    const item = items[dragIndex];
+    item.order = hoverIndex;
+    request({ url: `tasks/${item._id}`, method: "PUT", data: item });
 
-            const newItems = prevState.filter((i, idx) => idx !== dragIndex);
-            newItems.splice(hoverIndex, 0, item);
-            return [...newItems]
-        });
-    };
-    const buttonClicked =() =>  {refetch()}
+    setItems((prevState) => {
+      const newItems = prevState.filter((i, idx) => idx !== dragIndex);
+      newItems.splice(hoverIndex, 0, item);
+      return [...newItems];
+    });
+  };
+  const buttonClicked = () => {
+    refetch();
+  };
 
-   /* // for submitting add task for "in progress"
+  /* // for submitting add task for "in progress"
     const subbmitStuff = () => {
         // bind input field data : two way binding
         // pass BODY.stringify with binded data + status ('status:"in progress"')
     }
 */
-    return (
-        <div className={"row"}>
-            {statuses.map(s => (
-                
-                    <div key={s.status} className={"col-wrapper"}>
-                        <SubbmitCard 
-                        label="Add new card.." 
-                        type={"text"} 
-                        nameButton="Submit new card"
-                        status={s.status}
-                        projectId={projectId}
-                        buttonClicked={buttonClicked}
-                         />
-                        <h2 className={"col-header"}>{s.status.toUpperCase()}</h2>
-                        <DropWrapper onDrop={onDrop} status={s.status}>
-                            <Col>
-                                {items
-                                    .filter(i => i.status === s.status)
-                                    .map((i, idx) => <Item key={i.id} item={i} index={idx} moveItem={moveItem} status={s} projectId={projectId} />)}
-                            </Col>
 
-                        </DropWrapper>
-                    </div>
-                
-            ))}
 
+  return (
+    <>
+      <div className="w-full h-48">
+        <div className="mt-10 text-center">
+          <h2 className="text-3xl font-bold sm:text-4xl">Project Name</h2>
+          <h2 className="mt-5 text-xl sm:text-xl">Project description</h2>
+          <button
+           
+            className=" mt-5 px-5 py-2 text-white bg-indigo-600 border border-indigo-600 rounded hover:bg-transparent hover:text-indigo-600 active:text-indigo-500 focus:outline-none focus:ring"
+          >
+            Edit
+          </button>
+         
         </div>
-    )
-}
+      </div>
+      <div className="mx-10 mb-10 flex">
+        <div className={"row"}>
+          {statuses.map((s) => (
+            <div className="flex">
+              <div key={s.status} className={"col-wrapper"}>
+                <SubbmitCard
+                  label="New card"
+                  type={"text"}
+                  status={s.status}
+                  projectId={projectId}
+                  buttonClicked={buttonClicked}
+                />
+                <h2 className={"col-header"}>{s.status.toUpperCase()}</h2>
+                <DropWrapper onDrop={onDrop} status={s.status}>
+                  <Col>
+                    {items
+                      .filter((i) => i.status === s.status)
+                      .map((i, idx) => (
+                        <Item
+                          key={i.id}
+                          item={i}
+                          index={idx}
+                          moveItem={moveItem}
+                          status={s}
+                          projectId={projectId}
+                        />
+                      ))}
+                  </Col>
+                </DropWrapper>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+      <PopupProjectUpdate trigger={false}/>
+        
+    </>
+  );
+};
 export default ListOfTaskByProjectId;
